fix big bug: the assigner output should be processed by sigmoid rather softmax

src/transformer/attentionAssigner.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-import torch.nn.functional as F
+import torch
 
 from transformer.conv_encoder import Conv1d
 from utils.utils import sequence_mask
@@ -32,7 +32,7 @@ class Attention_Assigner(nn.Module):
         """
         x, input_lengths = self.conv(padded_input, input_lengths)
         alphas = self.linear(x).squeeze(-1)
-        alphas = F.softmax(alphas, -1)
+        alphas = torch.sigmoid(alphas)
         pad_mask = sequence_mask(input_lengths)
 
         return alphas * pad_mask

src/transformer/cif_model.py
@@ -17,12 +17,11 @@ class CIF_Model(nn.Module):
         self.encoder = encoder
         self.assigner = assigner
         self.decoder = decoder
+        self.ctc_fc = nn.Linear(encoder.dim_output, decoder.dim_output, bias=False)
 
         for p in self.parameters():
             if p.dim() > 1:
                 nn.init.xavier_uniform_(p)
-
-        self.ctc_fc = nn.Linear(encoder.dim_output, decoder.dim_output, bias=False)
 
     def forward(self, features, len_features, targets, threshold=0.95):
         """

src/transformer/conv_encoder.py
@@ -19,10 +19,12 @@ class Conv1d(nn.Module):
         self.name = name
 
         layers = [("{}/conv1d_0".format(name), nn.Conv1d(input_dim, hidden, context_width, 1)),
+                  ("{}/batchnorm_0".format(name), nn.BatchNorm1d(hidden)),
                   ("{}/relu_0".format(name), nn.ReLU())]
         for i in range(layer_num-1):
             layers += [
                 ("{}/conv1d_{}".format(name, i+1), nn.Conv1d(hidden, hidden, context_width, 1)),
+                ("{}/batchnorm_{}".format(name, i+1), nn.BatchNorm1d(hidden)),
                 ("{}/relu_{}".format(name, i+1), nn.ReLU())
             ]
         layers = OrderedDict(layers)
@@ -34,7 +36,6 @@ class Conv1d(nn.Module):
             feats = F.pad(feats, (0, 0, 0, self.layer_num * self.context_width))
         outputs = self.conv(feats.permute(0, 2, 1))
         outputs = outputs.permute(0, 2, 1)
-
 
         if self.pad == 'same':
             tensor_length = input_length